2d5a0f49ede03767ba2704b265356ff6ae2d2322
Sandeep Rupakheti <69429622+Sndpred@users.noreply.github.com>
2025-06-11 15:12:02 -0400

    Update ExploratoryDataAnalysis.sql

--- ExploratoryDataAnalysis.sql
+++ ExploratoryDataAnalysis.sql
@@ -1,10 +1,6 @@
 -- EDA
 
--- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
-
--- normally when you start the EDA process you have some idea of what you're looking for
-
--- with this info we are just going to look around and see what we find!
+-- Here we are going to explore the data and find trends or patterns or anything interesting like outliers
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
@@ -19,18 +15,18 @@ FROM world_layoffs.layoffs_staging2;
 
 
 
--- Looking at Percentage to see how big these layoffs were
+-- Looking at the Percentage to see how big these layoffs were
 SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off IS NOT NULL;
 
--- Which companies had 1 which is basically 100 percent of they company laid off
+-- Which companies had 1, which is basically 100 percent of they company laid off
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
--- these are mostly startups it looks like who all went out of business during this time
+-- these are mostly startups, it looks like who all went out of business during this time
 
--- if we order by funcs_raised_millions we can see how big some of these companies were
+-- If we order by funds_raised_millions we can see how big some of these companies were
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1
@@ -106,11 +102,8 @@ ORDER BY 2 DESC;
 
 
 
+-- Earlier, we looked at Companies with the most Layoffs. Now let's look at that per year. 
 
--- TOUGHER QUERIES------------------------------------------------------------------------------------------------------------------------------------
-
--- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
--- I want to look at 
 
 WITH Company_Year AS 
 (
